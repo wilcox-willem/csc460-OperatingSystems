20e2a655e6dcf3e279b2b97e2d256187af478e55
Willem Wilcox <11026542@live.mercer.edu>
2024-02-27 22:51:47 +0000

    viewList modifications

--- grocery/viewList.c
+++ grocery/viewList.c
@@ -1,50 +1,47 @@
+// Willem Wilcox
+// CSC 460, Dr. Allen
+// 2/24/24
+// grocery: viewList
+//  - started with a copy of "watch.c" from the pub folder
+
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <stdio.h>
 #include <sys/shm.h>
 
-struct bbStruct
-{
+// master struct, 
+struct bbStruct {
         int id;
         char name[20];
         int favNum;
         char favFood[30];
 };
 
-main(int argc, char *argv[])
-{
-int shmid,i;
-struct bbStruct *shmem;
-FILE *fopen(), *fp;
+main(int argc, char *argv[]) {
+    int shmid,i;
+    struct bbStruct *shmem;
+    FILE *fopen(), *fp;
 
-
-/*****  Open File to hold Shared Memory ID  *****/
-
-if((fp = fopen("./BBID.txt","r")) == NULL)
-    {
-    printf("watch:  could not open file.\n");
-    return(0);
+    /*****  Open File to hold Shared Memory ID  *****/
+    if((fp = fopen("./BBID.txt","r")) == NULL) {
+        printf("watch:  could not open file.\n");
+        return(0);
     }
 
-
-/*****  Get Shared Memory ID and close file  *****/
-
+    /*****  Get Shared Memory ID and close file  *****/
     fscanf(fp,"%d",&shmid);
     fclose(fp);
 
-/****   Attach to the shared memory  ******/
+    /****   Attach to the shared memory  ******/
+    shmem = (struct bbStruct *) shmat(shmid, NULL, SHM_RND);
 
-shmem = (struct bbStruct *) shmat(shmid, NULL, SHM_RND);
-
-printf("\n\nHERE IS OUR SHARED GROCERY LIST:\n\n");
-for (i=0;i<22;i++)
-    {
-       printf("%2d: %20s| %8d| %30s|\n",shmem->id,shmem->name,shmem->favNum,shmem->favFood);
-       shmem++;
+    printf("\n\nHERE IS OUR SHARED GROCERY LIST:\n\n");
+    for (i=0;i<22;i++) {
+        printf("%2d: %20s| %8d| %30s|\n",shmem->id,shmem->name,shmem->favNum,shmem->favFood);
+        shmem++;
     }
-printf("\n\n");
 
-
+    printf("\n\n");
 }
 
 
